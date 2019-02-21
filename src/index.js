e6a92c0086b82e69d144ea703c4a3b668e50fdac
Jonathan Freeman <freethejazz@gmail.com>
2019-02-20 21:29:17 -0600

    Adds tag retrieval

--- src/index.js
+++ src/index.js
@@ -3,28 +3,66 @@ import meow from 'meow';
 import chalk from 'chalk';
 import ora from 'ora';
 import terminalLink from 'terminal-link';
+import Table from 'cli-table';
+
+const addLatestTags = (image) => {
+  return axios.get(
+    `https://hub.docker.com/v2/repositories/library/${image.slug}/tags/`,
+    {
+      params: {
+        page_size: 5,
+        page: 1
+      }
+    }).then((res) => {
+      return {
+        ...image,
+        tags: res.data.results,
+      };
+    });
+};
+
+const generateTagTable = (tags) => {
+  const table = new Table({
+    head: ['Tag', 'Last Updated']
+  });
+  tags.forEach((tag) => {
+    table.push([tag.name, tag.last_updated])
+  });
+  return table.toString();
+};
 
 export const run = async () => {
   const cli = meow(`
-Usage
-  $ dlook <search-term>
+    Usage
+      $ dlook <search-term>
 
-Examples
-  $ dlook graph database
-  Found 4 results for graph database
+    Options
+      --with-latest-tags, -t Return 5 latest tags for each results
 
-  ${chalk.bold('neo4j')}
-  Neo4j is a highly scalable, robust native graph database.
+    Examples
+      $ dlook graph database
+      Found 4 results for graph database
 
-  ${chalk.bold('r-base')}
-  R is a system for statistical computation and graphics.
+      ${chalk.bold('neo4j')}
+      Neo4j is a highly scalable, robust native graph database.
 
-  ${chalk.bold('orientdb')}
-  OrientDB a Multi-Model Open Source NoSQL DBMS that combines graphs and documents.
+      ${chalk.bold('r-base')}
+      R is a system for statistical computation and graphics.
 
-  ${chalk.bold('arrangodb')}
-  ArangoDB - a distributed database with a flexible data model for documents, graphs, and key-values.
-`);
+      ${chalk.bold('orientdb')}
+      OrientDB a Multi-Model Open Source NoSQL DBMS that combines graphs and documents.
+
+      ${chalk.bold('arrangodb')}
+      ArangoDB - a distributed database with a flexible data model for documents, graphs, and key-values.
+    `,
+    {
+      flags: {
+        withLatestTags: {
+          type: 'boolean',
+          alias: 't',
+        },
+      },
+    });
 
   if (cli.input.length === 0) {
     console.error('Please include search terms.');
@@ -42,20 +80,45 @@ Examples
 
   const printResult = (image) => {
     const formattedName = chalk.bold(image.name)
-    const linkedName = terminalLink(formattedName, `https://hub.docker.com/_/${image.name}`)
-    console.log(linkedName);
-    console.log(`${image.short_description}\n`);
+    const linkedName = terminalLink(
+      formattedName,
+      `https://hub.docker.com/_/${image.slug}`
+    );
+    console.log(`${linkedName} - ${image.slug}`);
+    console.log(`${image.short_description}`);
+    if (image.tags) {
+      console.log(generateTagTable(image.tags))
+    }
+    console.log(``);
   };
 
   const spinner = ora(`Searching '${searchString}' on Docker Hub`).start();
 
-  axios.get('https://store.docker.com/api/content/v1/products/search', { params })
+  axios.get('https://hub.docker.com/api/content/v1/products/search', {
+    params,
+    headers: {
+      'Search-Version': 'v3',
+    },
+  })
     .then((response) => {
       const results = response.data;
       spinner.stop();
       if (results.count > 0) {
-        console.log(`Found ${results.count} results for ${searchString}\n`);
-        results.summaries.forEach(printResult);
+        console.log(`Showing ${Math.min(25, results.count)} of ${results.count} results for ${searchString}\n`);
+        if (cli.flags.withLatestTags) {
+          const spinner2 = ora(`Getting tags for ${Math.min(25, results.count)} results`).start();
+          Promise.all(results.summaries.map(addLatestTags))
+            .then((images) => {
+              spinner2.stop();
+              images.forEach(printResult);
+            }).catch((error) => {
+              spinner2.stop();
+              console.error(`Failed to search get tags`);
+              results.summaries.forEach(printResult);
+            });
+        } else {
+          results.summaries.forEach(printResult);
+        }
       } else {
         console.log(`Could not find any images matching ${searchString}`);
       }
@@ -64,5 +127,4 @@ Examples
       console.error(`Failed to search for ${searchString}`);
       console.error(error);
     });
-
 }
